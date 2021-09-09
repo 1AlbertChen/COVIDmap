bbc2d8fe3ef799bdbd25e21826e4d35aa5a4dd8b
1AlbertChen <zhenrongchen2021@u.northwestern.edu>
2021-09-09 22:08:23 +0800

    Jquery Implementation

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 const baseURL = 'https://api.covid19api.com/total/dayone/country/';
 
 const search = (ev) => {
-    const term = document.querySelector('#search').value;
+    const term = $('#search').val();
     getCases(term);
     if (ev) {
         ev.preventDefault();
     }
 }
-document.querySelector('#search').onkeyup = (ev) => {
-    if (ev.keyCode === 13) {
-        ev.preventDefault();
-        search();
+$('#search').keyup(function( event ) {
+    if ( event.which == 13 ) {
+      event.preventDefault();
+      search();
     }
-};
+  });
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
@@ -22,20 +22,21 @@ const getCases = (term) => {
      .then(response=>response.json())
      .then(data=>{
         if (data.length==0){
-            document.querySelector("#cases").innerHTML=``;
-            document.querySelector("#deaths").innerHTML=``;
+            $("#cases").html('');
+            $("#deaths").html('');
         }
         else{
             template1=`<b>${numberWithCommas(data[data.length-1].Confirmed)}</b> 
             <small>(+${numberWithCommas(data[data.length-1].Confirmed-data[data.length-2].Confirmed)})</small>`;
-            document.querySelector("#cases").innerHTML=template1;
+            $("#cases").html(template1);
             template2=`<b>${numberWithCommas(data[data.length-1].Deaths)}</b> 
             <small>(+${numberWithCommas(data[data.length-1].Deaths-data[data.length-2].Deaths)})</small>` ;
-            document.querySelector("#deaths").innerHTML=template2;
+            $("#deaths").html(template2);
             template3=`<b>${data[data.length-1].Country}</b>`;
-            document.querySelector("#country").innerHTML=template3;
+            $("#country").html(template3);
         }
      })
 };
+
 
 
