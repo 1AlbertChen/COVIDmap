34fb782e27c1ffad96a96c832fa15e4f3c109e33
1AlbertChen <zhenrongchen2021@u.northwestern.edu>
2021-10-04 23:30:59 +0800

    API Updates

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,8 +1,37 @@
-const baseURL = 'https://api.covid19api.com/total/dayone/country/';
+const baseURL = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/covid-19-qppza/service/REST-API/incoming_webhook/';
 
-const countries = ['United States','China','UK']
-const countrySet = new Set()
-for (let item of countries) countrySet.add(item)
+const countries = ['US', 'China','United Kingdom','France','Russia'];
+const countrySet = new Set();
+for (let item of countries) countrySet.add(item);
+
+var d = new Date();
+d.setDate(d.getDate()-1)
+var dd = String(d.getDate()).padStart(2, '0');
+var mm = String(d.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = d.getFullYear();
+
+var lastDay = yyyy + '-' + mm + '-' + dd;
+
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+
+for (let item of countrySet){
+    url= baseURL+ 'countries_summary?country=' + item + '&min_date=' + lastDay + 'T00:00:00.000Z';
+    fetch(url)
+        .then(response=>response.json())
+        .then(data=>{
+            var cases = numberWithCommas(data[0].confirmed)
+            var deaths = numberWithCommas(data[0].deaths)
+            var newCases = numberWithCommas(data[0].confirmed_daily)
+            var newDeaths = numberWithCommas(data[0].deaths_daily)
+            var country = data[0].country
+            $("#carousel-frame").append(`<div class="carousel-item text-white" data-bs-interval="3000"><b>${country}</b><b class="text-secondary"> | Cases: </b><b style="color:red;">${cases} </b><small style="color:red;">(+${newCases})</small><b class="text-secondary"> Deaths: </b><b>${deaths} </b><small>(+${newDeaths})</small>`)
+        });
+}
+    
 
 const search = (ev) => {
     const term = $('#search').val();
@@ -17,27 +46,23 @@ $('#search').keyup(function(event) {
       search();
     }
   });
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
+
 const getCases = (term) => {
-    url= baseURL + term;
+    url= baseURL+ 'countries_summary?country=' + term + '&min_date=' + lastDay + 'T00:00:00.000Z';
     fetch(url)
      .then(response=>response.json())
      .then(data=>{
         if (data.length==0){
             $("#cases").html('');
             $("#deaths").html('');
+            $("country").html('');
         }
         else{
-            var thisDay = data[data.length-1]
-            var lastDay = data[data.length-2]
-            var country = thisDay.Country
-            var cases = numberWithCommas(thisDay.Confirmed)
-            var deaths = numberWithCommas(thisDay.Deaths)
-            var newCases = numberWithCommas(thisDay.Confirmed - lastDay.Confirmed)
-            var newDeaths = numberWithCommas(thisDay.Deaths - lastDay.Deaths)
-
+            var cases = numberWithCommas(data[0].confirmed)
+            var deaths = numberWithCommas(data[0].deaths)
+            var newCases = numberWithCommas(data[0].confirmed_daily)
+            var newDeaths = numberWithCommas(data[0].deaths_daily)
+            var country = data[0].country
             template1=`<b>${cases}</b> 
             <small>(+${newCases})</small>`;
             $("#cases").html(template1);
@@ -56,21 +81,6 @@ const getCases = (term) => {
 };
 
 
-for (let item of countrySet){
-    countryURL= baseURL + item;
-    fetch(countryURL)
-        .then(response=>response.json())
-        .then(data=>{
-            var thisDay = data[data.length-1]
-            var lastDay = data[data.length-2]
-            var country = thisDay.Country
-            var cases = numberWithCommas(thisDay.Confirmed)
-            var deaths = numberWithCommas(thisDay.Deaths)
-            var newCases = numberWithCommas(thisDay.Confirmed - lastDay.Confirmed)
-            var newDeaths = numberWithCommas(thisDay.Deaths - lastDay.Deaths)
-            $("#carousel-frame").append(`<div class="carousel-item text-white"><b>${country}</b><b class="text-secondary"> | Cases: </b><b style="color:red;">${cases} </b><small style="color:red;">(+${newCases})</small><b class="text-secondary"> Deaths: </b><b>${deaths} </b><small>(+${newDeaths})</small>`)
-        });
-}
 
 
 
